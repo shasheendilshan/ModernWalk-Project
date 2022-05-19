4e0d7afc15c880dfffa671c715a43c8fb9d3df3e
unknown <shasheen@i-threesixty.com>
2022-05-19 16:01:32 +0530

    fix: Modify getSpecificCategory function in  Api

--- src/api/index.ts
+++ src/api/index.ts
@@ -27,20 +27,6 @@ export const getAllProducts = async () => {
       return Response;
     }
   };
-
-  // export const getAllProducts = async () => {
-  //   try {
-  //     const allProducts = await axios.get(
-  //       "https://fakestoreapi.com/products?limit=18"
-  //     );
- 
-  //     return { data: allProducts.data, error: null};
-  //   } catch (error:any) {
-  //     console.log(error.response);
-  //     return { data: [], error: error.response };
-  //   }
-  // };
-  
   
   export const getSpecificCategory = async (category:string, limit?:number) => {
     try {
@@ -49,10 +35,19 @@ export const getAllProducts = async () => {
           limit ? "?limit=" + limit : ""
         }`
       );
-      return { data: allProducts.data, error: 0 };
+
+     const Response:Response = {
+        data: allProducts.data,
+        error: null
+      }   
+      return Response;
     } catch (error:any) {
       console.log(error.response);
-      return { data: [], error: error.response };
+      const Response:Response = {
+        data: [],
+        error: null
+      }  
+      return Response;
     }
   };
   
