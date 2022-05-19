c5e55c5d9db7600a4f95120c69e870dd64ca57fc
unknown <shasheen@i-threesixty.com>
2022-05-19 10:42:51 +0530

    feat: Add navigation link to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navbar:React.FC = () => {
   return (
     <div className="w-[100%] h-[70px] z-20 bg-white drop-shadow-lg fixed top-0">
     <div className="flex items-center justify-center h-full ">
+    <Link to="/">
         <h1 className="text-3xl md:text-4xl font-bold text-slate-700">
           Modern Walk
         </h1>
+     </Link>
     </div>
   </div>
   )
